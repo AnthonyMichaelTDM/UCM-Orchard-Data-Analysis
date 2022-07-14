777db3afe5907b4d2f65940d67f2daba10a4a05a
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2022-07-14 16:17:36 -0700

    rename data_analyzer

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import os
 from data_parser import Parser, File_Data_Source
-from data_analyzer import Processor
+from data_processor import Processor
 from definitions import ROOT_DIR
 
 # from here, call other scripts to choose and analyze data
@@ -19,8 +19,9 @@ then do stuff with the data, ie. plot it on a graph
 
 #show off milestone 1
 file_path = os.path.join(ROOT_DIR, 'data', 'Almond_data_20220610', 'Data_TREWid1_22_04_almond.csv')
-data = Parser.parse( file_path, File_Data_Source.SAP_AND_MOISTURE_SENSOR)
-processor = Processor(data, File_Data_Source.SAP_AND_MOISTURE_SENSOR)
+file_source = File_Data_Source.SAP_AND_MOISTURE_SENSOR
+data = Parser.parse( file_path, file_source)
+processor = Processor(data, file_source)
 processor.remove_fields(['Field', 'Sensor ID'])
 processor.keep_time_range(datetime(year=2022, month=4, day=30, hour=12),datetime(year=2022, month=4, day=30, hour=13))
 
