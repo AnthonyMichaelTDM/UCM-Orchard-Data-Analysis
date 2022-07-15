b0bdd421b81e13151aa934c07685b8916c51dae7
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2022-07-14 19:45:35 -0700

    fix coefficients

--- src/other_scripts/calc_calibration_coefficients.py
+++ src/other_scripts/calc_calibration_coefficients.py
@@ -7,7 +7,7 @@ from numpy import array, average
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 idnum = 1
-file_path = os.path.join(ROOT_DIR, 'data', 'Almond_data_20220610', 'Calibration_TREWid{}_almond.csv'.format(idnum))
+file_path = os.path.join(ROOT_DIR, 'data', 'Calibration_TREWid{}_almond.csv'.format(idnum))
 
 while os.path.isfile(file_path):
     data=[]
@@ -23,14 +23,14 @@ while os.path.isfile(file_path):
             data = [ [int(data) for data in row] for row in reader[1:]] #convert data to int, also skip first row
 
     #calculate coefficients
-    ave_wet = average( array(data[0]) )
-    ave_dry = average( array(data[1]) )
+    ave_wet = average( [row[0] for row in data] )
+    ave_dry = average( [row[1] for row in data] )
     
-    a = 100/(ave_wet-ave_dry) #  (y2-y1) / (x2-x1)
-    b = -a * ave_dry # y=0=ax+b ->  b=-ax
+    a = (100-0)/(ave_wet-ave_dry) #  (y2-y1) / (x2-x1)
+    b = 100-(a*ave_wet) # y=ax+b ->  b=y-ax
     
-    print("Sensor {id}:\n\ta = {a}\n\tb = {b}\n".format(id=idnum,a=a,b=b))
+    print("Sensor {id}:\n\ta = {a}\n\tb = {b}\n\tave wet (max) = {wet}\n\tave dry (min) = {dry}".format(id=idnum,a=a,b=b,wet=ave_wet,dry=ave_dry))
     
     #prep for next loop
     idnum += 1
-    file_path = os.path.join(ROOT_DIR, 'data', 'Almond_data_20220610', 'Calibration_TREWid{}_almond.csv'.format(idnum))
+    file_path = os.path.join(ROOT_DIR, 'data', 'Calibration_TREWid{}_almond.csv'.format(idnum))
