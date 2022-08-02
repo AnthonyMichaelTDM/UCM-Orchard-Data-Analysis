db7b9f9feb5a30d0bb2d5dd8284da77c54a47c39
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2022-08-02 11:39:10 -0700

    added note for future maintainers

--- src/main.py
+++ src/main.py
@@ -7,10 +7,14 @@ from definitions import Configs, Data_Sensor_Type
 
 from wrapper import Wrapper
 
+## NOTE for future maintainers:
+## the primary potential optomization is to utilize data-frames instead of dictionaries as they can access elements faster
+## beyond that, trimming data sooner and algorithm optomizations where possible may help too.
+
 
 ##uncomment if you want to hardcode the time range rather than entering it in manually
-Wrapper.run(Configs.PISTACHIO, datetime(2022,7,28,0,0), datetime(2022,8,1,23,59), sap_sensorid=1, weather_sensorid=2, lux_sensorid=1)
-exit()
+#Wrapper.run(Configs.PISTACHIO, datetime(2022,7,28,0,0), datetime(2022,8,1,23,59), sap_sensorid=1, weather_sensorid=2, lux_sensorid=1)
+#exit()
 
 #TODO: add let user choose between almond and pistaccio data, then if pistaccio have them choose the weather station id (basically build Config with user input)
 def get_int(prompt:str, min:int, max:int) -> int:
